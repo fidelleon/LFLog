fc23f77facad03fcc1cd959e1c9b3793e8f44600
Fidel Leon <fidel@flm.cat>
2025-08-03 19:20:56 +0200

    Tools: locators, add comments for use of geographiclib library

--- tools/locators.py
+++ tools/locators.py
@@ -1,6 +1,20 @@
 import re
 
 
+# from geographiclib.geodesic import Geodesic
+# Geodesic.WGS84.Inverse(p1.latitude, p1.longitude, p2.latitude, p2.longitude)
+# {'lat1': 41.395883, 'lon1': 2.041667, 'lat2': 41.520833, 'lon2': 2.458333, 'a12': 0.3372916433849669,
+# 's12': 37476.27220013839, 'azi1': 68.12834904653883, 'azi2': 68.40421426320225}
+# lat1 = φ1, latitude of point 1 (degrees)
+# lon1 = λ1, longitude of point 1 (degrees)
+# azi1 = α1, azimuth of line at point 1 (degrees)
+# lat2 = φ2, latitude of point 2 (degrees)
+# lon2 = λ2, longitude of point 2 (degrees)
+# azi2 = α2, (forward) azimuth of line at point 2 (degrees)
+# s12 = s12, distance from 1 to 2 (meters)
+# a12 = σ12, arc length on auxiliary sphere from 1 to 2 (degrees)
+
+
 def maidenhead_to_coordinates(locator: str, grid_center: bool = True) -> (float, float):
     """
     Returns a tuple with the latitude and longitude of the Maidenhead locator
